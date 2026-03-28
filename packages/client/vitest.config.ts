f0d3c978f4b89a865fddf9437d0d1ec43c70c5ad
Dennis Mouwen <dennis.mouwen@eye.security>
2026-03-28 12:19:38 +0100

    fix: update vitest config to reference setup.tsx

--- packages/client/vitest.config.ts
+++ packages/client/vitest.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     root: "./src",
-    setupFiles: ["./src/test/setup.ts"],
+    setupFiles: ["./src/test/setup.tsx"],
   },
 });
